cb7f0ed3ec1528a0fda8839e82029dd10a780ce8
OctoBanon <82901151+OctoBanon-Main@users.noreply.github.com>
2022-04-01 01:34:07 +0700

    Add files via upload

---
+++ files/10/activate.bat
@@ -0,0 +1,24 @@
+@echo off
+
+:loop
+
+echo .bat file for automated activation Windows 10 LTSC
+echo By Sleeping Cat
+timeout 5 /nobreak
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+cd /d "%~dp0"
+xcopy /e/y skus %windir%\system32\spp\tokens\skus
+cscript.exe %windir%\system32\slmgr.vbs /rilc
+cscript.exe %windir%\system32\slmgr.vbs /upk >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ckms >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /cpky >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+cscript.exe %windir%\system32\slmgr.vbs /skms kms.teevee.asia
+cscript.exe %windir%\system32\slmgr.vbs /ato
+taskkill /f /im explorer.exe & start explorer.exe
